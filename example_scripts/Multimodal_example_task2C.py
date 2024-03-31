3e93abe3cf8805b68923bf6b6c9074558c182a03
Kevin Mathew T <mathewtkevin@gmail.com>
2024-04-01 01:15:50 +0530

    new arch

--- example_scripts/Multimodal_example_task2C.py
+++ example_scripts/Multimodal_example_task2C.py
@@ -430,7 +430,7 @@ class MultimodalClassifier(nn.Module):
         ]
 
     def forward(self, text, image, mask):
-        text_output = self.text_model(text, attention_mask=mask).last_hidden_state
+        text_output = self.text_model(text, attention_mask=mask)
         text_output = self.text_dropout(text_output[:, 0, :])
         text_output = self.text_fc(text_output)
 
