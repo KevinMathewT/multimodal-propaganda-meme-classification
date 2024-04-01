8dedd6e24e7433183057f07924fb0e11552598d8
Kevin Mathew T <mathewtkevin@gmail.com>
2024-04-01 23:19:27 +0530

    new arch

--- example_scripts/Multimodal_example_task2C.py
+++ example_scripts/Multimodal_example_task2C.py
@@ -35,9 +35,9 @@ from transformers import AutoTokenizer, BertTokenizer
 from sklearn.metrics import f1_score
 from transformers import get_linear_schedule_with_warmup
 
-# text_model = "aubmindlab/bert-base-arabertv2"
+text_model = "aubmindlab/bert-base-arabertv2"
 # text_model = "distilbert-base-multilingual-cased"
-text_model = "FacebookAI/xlm-roberta-base"
+# text_model = "FacebookAI/xlm-roberta-base"
 # text_model = 'CAMeL-Lab/bert-base-arabic-camelbert-mix-pos-egy'
 image_model = "efficientnet_b5"
 # image_model = "resnet50"
