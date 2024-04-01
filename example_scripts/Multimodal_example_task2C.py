581288f6ad8ba341aae73cd75e735f97ffcf8b1b
Kevin Mathew T <mathewtkevin@gmail.com>
2024-04-01 14:37:37 +0530

    new arch

--- example_scripts/Multimodal_example_task2C.py
+++ example_scripts/Multimodal_example_task2C.py
@@ -191,7 +191,7 @@ train_df = torch.utils.data.DataLoader(
     train_df, batch_size=batch_size, shuffle=True, drop_last=True
 )
 validation_df = torch.utils.data.DataLoader(
-    validation_df, batch_size=batch_size, shuffle=True, drop_last=True
+    validation_df, batch_size=batch_size, shuffle=True, drop_last=False
 )
 
 import torch
