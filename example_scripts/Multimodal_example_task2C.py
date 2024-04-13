61217d2283302108c24547525503cb99da7203ef
Kevin Mathew T <mathewtkevin@gmail.com>
2024-04-13 16:54:25 +0530

    new arch

--- example_scripts/Multimodal_example_task2C.py
+++ example_scripts/Multimodal_example_task2C.py
@@ -115,7 +115,6 @@ def setup(fold):
 
     test_df = read_data(test_file)
 
-    print(train_df)
     train_df.loc[:, "label"] = train_df["label"].map(l2id)
     val_df.loc[:, "label"] = val_df["label"].map(l2id)
     test_df["label"] = test_df["label"].map(l2id)
